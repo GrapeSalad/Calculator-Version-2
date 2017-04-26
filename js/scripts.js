5244d6fcf3aa4b7e0b7323b20e00a5c65336dc32
David Wilson and Jordan Loop <david@>
2017-04-26 11:27:25 -0700

    remove popup dialogue, add user-interface functionality, make pretty.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,34 @@
-
-
-
+//BUSINESS LOGIC
 var add = function(number1, number2) {
   return number1 + number2;
 };
-
 var subtract = function(number1, number2){
   return number1 - number2;
 };
-
 var multiply = function(number1, number2){
   return number1 * number2;
 };
-
 var divide = function(number1, number2){
   return (number1 / number2).toFixed(3);
 };
-
 var modulus = function(number1, number2){
   return number1 % number2;
 };
-
 var squareroot = function(number1){
   return Math.sqrt(number1).toFixed(3);
 };
+//INTERFACE LOGIC
+$(document).ready(function() {
+  $("form#numberEntry").submit(function(event){
+    event.preventDefault();
 
-// USER PROMPTS
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
 
-
-
-var addResult = add(number1, number2);
-var subtractResult = subtract(number1, number2);
-var multiplyResult = multiply(number1, number2);
-var divideResult = divide(number1, number2);
-var modulusResult = modulus(number1, number2);
-var sqrtResult = squareroot(number1);
-
-
-alert("Adding these results in " + addResult);
-alert("Subtracting these results in " + subtractResult);
-alert("Multiplying these results in " + multiplyResult);
-alert("Dividing these results in " + divideResult);
-alert("The remainder (modulus) of these results is " + modulusResult);
-alert("The square root of this number is " + sqrtResult);
+    $("#outputAdd").text("Adding these results in " + add(number1, number2));    $("#outputSubtract").text("Subtracting these results in " + subtract(number1, number2));
+    $("#outputMultiply").text("Multiplying these results in " + multiply(number1, number2));
+    $("#outputDivide").text("Dividing these results in " + divide(number1, number2));
+    $("#outputModulus").text("The remainder (modulus) of these results is " + modulus(number1, number2));
+    $("#outputSqrRt").text("The square root of this number is " + squareroot(number1));
+  });
+});
